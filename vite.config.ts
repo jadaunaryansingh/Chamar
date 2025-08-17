e12c1f4aab280e3055d432219ab053f813806dcd
jadaunaryansingh <coderaryanjadaun19@outlook.com>
2025-08-18 00:51:16 +0530

    D

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: './',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  },
   server: {
     port: 5173,
     host: true
